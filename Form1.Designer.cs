7185ba2e5ae3be3e7b6ef025b13da7bbce5b42ba
Michael Melikov <coutaq@gmail.com>
2020-11-20 00:27:19 +0300

    done

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,11 +29,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.drawGraphic = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.Transparent;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.drawGraphic, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1484, 961);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // drawGraphic
+            // 
+            this.drawGraphic.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.drawGraphic.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.drawGraphic.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.drawGraphic.FlatAppearance.BorderSize = 0;
+            this.drawGraphic.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DarkTurquoise;
+            this.drawGraphic.FlatAppearance.MouseOverBackColor = System.Drawing.Color.MediumTurquoise;
+            this.drawGraphic.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.drawGraphic.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.drawGraphic.Location = new System.Drawing.Point(542, 887);
+            this.drawGraphic.MaximumSize = new System.Drawing.Size(400, 50);
+            this.drawGraphic.Name = "drawGraphic";
+            this.drawGraphic.Size = new System.Drawing.Size(400, 50);
+            this.drawGraphic.TabIndex = 0;
+            this.drawGraphic.Text = "Вывести график температур";
+            this.drawGraphic.UseVisualStyleBackColor = false;
+            this.drawGraphic.Click += new System.EventHandler(this.drawGraphic_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1478, 858);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1484, 961);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
         }
+
+        private System.Windows.Forms.Button drawGraphic;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
 
         #endregion
     }
--- Form1.cs
+++ Form1.cs
@@ -1,20 +1,63 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+// ReSharper disable PossibleLossOfFraction
 
 namespace lab_four_graphics
 {
     public partial class Form1 : Form
     {
+        private readonly int[] _temperatures =
+        {
+            5, 11, 12, 18, 2, -6, -8, 5, 8, 16, 21, 17, 0, -1, 4, -6,
+            3, 3, 3, 0, -2, -8, 0, 17, 7, 9, 16, 10, 16, 19, 12
+        };
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void drawGraphic_Click(object sender, EventArgs e)
+        {
+            var g = pictureBox1.CreateGraphics();
+            g.Clear(Color.White);
+            var max = _temperatures.Concat(new[] {-1}).Max();
+            var size = pictureBox1.Size;
+            var dy = size.Height/(2*max);
+            var dx = size.Width/31;
+            var p = new Pen(Color.Navy);
+            var fn = new Font("Segoe UI", 10, FontStyle.Bold, GraphicsUnit.Point);
+            var offset = g.MeasureString(Convert.ToString(max), fn);
+            var sf = new StringFormat
+            {
+                Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Center
+            };
+            
+            g.DrawLine(p, offset.Width, size.Height/2, size.Width, size.Height/2);
+            g.DrawLine(p, offset.Width, 0, offset.Width, size.Height);
+            //текст на оси y
+            for (int i = 0, y = size.Height/2; i <= max; i++) {
+                g.DrawString(Convert.ToString(i), fn, Brushes.Tomato,10, y-dy*i, sf);
+                g.DrawLine(p, offset.Width, y-dy*i, size.Width, y-dy*i);
+                
+                g.DrawString(Convert.ToString(i), fn, Brushes.Tomato,10, y+dy*i, sf);
+                g.DrawLine(p, offset.Width, y+dy*i, size.Width, y+dy*i);
+            }
+            // текст на оси x
+            for (int i = 1, x = dx; i <= _temperatures.Length; i++, x += dx) {
+                g.DrawString(Convert.ToString(i), fn, Brushes.Tomato, x,(size.Height-offset.Height-2)/2, sf);
+                g.DrawLine(p, x, size.Height / 2 - 5 , x,
+                size.Height/2+5);
+            }
+            p.Dispose();
+            p = new Pen(Color.Teal, 3) {DashStyle = System.Drawing.Drawing2D.DashStyle.Dot};
+            g.DrawEllipse(Pens.Tomato, dx-3, pictureBox1.Size.Height/2-_temperatures[0]*dy-3, 6, 6);
+            for (int i = 1, x = dx; i <= _temperatures.Length-1; i++, x+=dx) {
+                g.DrawLine(p, x, pictureBox1.Size.Height/2-_temperatures[i-1]*dy, x+dx, pictureBox1.Size.Height/2-_temperatures[i]*dy);
+                g.DrawEllipse(Pens.Tomato, x+dx-3,pictureBox1.Size.Height/2-_temperatures[i]*dy-3, 6, 6);
+            }
+            p.Dispose();
         }
     }
 }
